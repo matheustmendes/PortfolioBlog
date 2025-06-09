caa51659a1f87604d614eaecaa93d73814970676
matheus <matios.torrres@gmail.com>
2025-06-08 22:46:40 -0300

    Created Post Class and PostService class w/ AddPost, RemovePost, UpdatePost. Next step: implement on Console Menu for interaction

---
+++ BlogPortfolio/Post.cs
@@ -0,0 +1,11 @@
+﻿namespace BlogPortfolio;
+
+public class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    public string Author { get; set; } = string.Empty;
+}
---
+++ BlogPortfolio/PostService.cs
@@ -0,0 +1,59 @@
+﻿namespace BlogPortfolio;
+
+public class PostService
+{
+    List<Post> posts = new List<Post>();
+
+    public void AddPost(Post post)
+    {
+        if (post == null)
+        {
+            throw new ArgumentNullException(nameof(post), "Post cannot be null");
+        }
+
+        post.Id = posts.Count + 1; // Simple ID assignment
+        posts.Add(post);
+    }
+
+    public List<Post> GetAllPosts()
+    {
+        return posts; 
+    }
+    
+    public void RemovePost(int id) 
+    {
+        var post = posts.FirstOrDefault(p => p.Id == id);
+        if (post != null)
+        {
+            posts.Remove(post);
+        }
+        else
+        {
+            throw new KeyNotFoundException($"Post with ID {id} not found.");
+        }
+    }
+    
+    // talvez implementar depois um getpostbyid 
+    
+    // metodo para editar post:
+    
+    // Um método não pode ter seu id atualizado, nem seu autor modificado.
+
+    public void UpdatePost(int id, Post UpdatedPost)
+    {
+        if (UpdatedPost == null)
+        {
+            throw new ArgumentNullException(nameof(UpdatedPost), "Post cannot be null");
+        }
+
+        var post = posts.FirstOrDefault((p) => p.Id == id);
+        if (post == null)
+        {
+            throw new KeyNotFoundException($"Post with ID {id} not found.");
+        }
+        
+        post.Title = UpdatedPost.Title;
+        post.Content = UpdatedPost.Content;
+        post.UpdatedAt = DateTime.Now;
+    }
+}
